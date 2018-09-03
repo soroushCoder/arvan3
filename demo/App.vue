b4a7e5730497c9498e2949238cca42693a592912
soroushCoder <soroush.e89@gmail.com>
2018-09-03 16:36:21 +0430

    first commit

---
+++ demo/App.vue
@@ -0,0 +1,125 @@
+<template>
+  <div id="app" class="flyout">
+    <div class="header-arvan">
+    <navbar dark position="top" class="" scrolling>
+      <span class="arvan-brand">
+        D
+      </span>
+      <navbar-collapse>
+        <navbar-nav right>
+          <navbar-item href="#/" waves-fixed>Home</navbar-item>
+          <navbar-item href="#" waves-fixed>About</navbar-item>
+          <navbar-item href="#" waves-fixed>Screen</navbar-item>
+          <navbar-item href="#" waves-fixed>Contact</navbar-item>
+          <button class="Create-Button">Create account</button>
+        </navbar-nav>
+      </navbar-collapse>
+    </navbar>
+      <div class="row top-position">
+        <div class="col-md-6">
+          <p class="bolder">
+            Manage All of Your Tasks in One Place.
+          </p>
+          <p >
+           Nulla, euismod ...h
+          </p>
+          <button class="Learn-Button">Learn More</button>
+        </div>
+        <div class="col-md-6">
+
+          <img src="./assets/mobile.png" height="513" width="280"/></div>
+      </div>
+    </div>
+    <main :style="{marginTop: '60px'}">
+      <router-view></router-view>
+    </main>
+    <Ftr color="default-color">
+      <p class="footer-copyright mb-0 py-3 text-center">
+        &copy; {{new Date().getFullYear()}} Copyright: <a href="https://www.MDBootstrap.com"> MDBootstrap.com </a>
+      </p>
+    </Ftr>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+import NavbarItem from '@/components/NavbarItem.vue';
+import NavbarNav from '@/components/NavbarNav.vue';
+import NavbarCollapse from '@/components/NavbarCollapse.vue';
+import Ftr from '@/components/Footer.vue';
+import EdgHd from '@/components/EdgeHeader.vue';
+import mdbNavbarBrand from '@/components/NavbarBrand.vue';
+import { Btn } from 'mdbvue';
+
+export default {
+  name: 'app',
+  components: {
+    Navbar,
+    NavbarItem,
+    NavbarNav,
+    NavbarCollapse,
+    Ftr,
+    EdgHd,
+    mdbNavbarBrand,
+    Btn
+  }
+};
+
+</script>
+
+<style>
+.flyout {
+	display:flex;
+	flex-direction: column;
+	min-height:100vh;
+	justify-content: space-between;
+}
+  .Create-Button{
+
+    color: white;
+    border:1px solid white;
+    -webkit-border-radius: 10em;
+    border-radius: 10em;
+    background-color: inherit;
+  }
+  .header-arvan{
+    background-image: linear-gradient(to bottom right,#c96dd8, #3525af)!important;
+    height: 550px;
+  }
+.header-arvan .navbar{
+  -webkit-box-shadow: none;
+  box-shadow: none;
+}
+  .arvan-brand{
+    font-weight: 800;
+    font-size: x-large;
+    background-color: white;
+    border-radius: 30px;
+    color: #c66cd7!important;
+    height: 35px;
+    width: 35px;
+    text-align: center;
+  }
+  .top-position{
+    margin-top: 150px;
+    padding: 0 20%;
+  }
+  .top-position p {
+
+    width: 180px;
+    color: white;
+  }
+.top-position .bolder{
+  font-weight: bolder;
+  font-size: 20px;
+}
+  .Learn-Button{
+    color: #3e32b3;
+    -webkit-border-radius: 10em;
+    border-radius: 10em;
+    background-color: white;
+    padding: 12px;
+    border: 0;
+  }
+
+</style>
---
+++ demo/index.js
@@ -0,0 +1,227 @@
+// FREE
+import Badge, { mdbBadge } from './components/Badge';
+import Breadcrumb, { mdbBreadcrumb } from './components/Breadcrumb';
+import BreadcrumbItem, { mdbBreadcrumbItem } from './components/BreadcrumbItem';
+import Btn, { mdbBtn } from './components/Button';
+import BtnGroup, { mdbBtnGroup } from './components/ButtonGroup';
+import Card, { mdbCard } from './components/Card';
+import CardAvatar, { mdbCardAvatar } from './components/CardAvatar';
+import CardUp, { mdbCardUp } from './components/CardUp';
+import CardBody, { mdbCardBody } from './components/CardBody';
+import CardFooter, { mdbCardFooter } from './components/CardFooter';
+import CardHeader, { mdbCardHeader } from './components/CardHeader';
+import CardImg, { mdbCardImage } from './components/CardImg';
+import CardText, { mdbCardText } from './components/CardText';
+import CardTitle, { mdbCardTitle } from './components/CardTitle';
+import CardGroup, { mdbCardGroup } from './components/CardGroup';
+import Carousel, { mdbCarousel } from './components/Carousel';
+import CarouselCaption, { mdbCarouselCaption } from './components/CarouselCaption';
+import CarouselIndicator, { mdbCarouselIndicator } from './components/CarouselIndicator';
+import CarouselIndicators, { mdbCarouselIndicators } from './components/CarouselIndicators';
+import CarouselInner, { mdbCarouselInner } from './components/CarouselInner';
+import CarouselItem, { mdbCarouselItem } from './components/CarouselItem';
+import CarouselNavigation, { mdbCarouselControl } from './components/CarouselNavigation';
+import LineChart, { mdbLineChart } from './components/ChartLine';
+import RadarChart, { mdbRadarChart } from './components/ChartRadar';
+import BarChart, { mdbBarChart } from './components/ChartBar';
+import PolarChart, { mdbPolarChart } from './components/ChartPolar';
+import PieChart, { mdbPieChart } from './components/ChartPie';
+import DoughnutChart, { mdbDoughnutChart } from './components/ChartDoughnut';
+import Collapse, { mdbCollapse } from './components/Collapse';
+import Column, { mdbCol } from './components/Col';
+import Container, { mdbContainer } from './components/Container';
+import Datatable, { mdbDatatable } from './components/Datatable';
+let Dropdown = require('./components/Dropdown').default;
+let mdbDropdown = require('./components/Dropdown').default;
+import DropdownItem, { mdbDropdownItem } from './components/DropdownItem';
+import DropdownMenu, { mdbDropdownMenu } from './components/DropdownMenu';
+import DropdownToggle, { mdbDropdownToggle } from './components/DropdownToggle';
+import EdgeHeader, { mdbEdgeHeader } from './components/EdgeHeader';
+import Fa, { mdbIcon } from './components/Fa';
+import Footer, { mdbFooter } from './components/Footer';
+import HelloWorld, { mdbHelloWorld } from './components/HelloWorld';
+import Jumbotron, { mdbJumbotron } from './components/Jumbotron';
+import ListGroup, { mdbListGroup } from './components/ListGroup';
+import ListGroupItem, { mdbListGroupItem } from './components/ListGroupItem';
+import MdInput, { mdbInput } from './components/MdInput';
+import MdMask, { mdbMask } from './components/Mdmask';
+import MdTextarea, { mdbTextarea } from './components/MdTextarea';
+import Media, { mdbMedia } from './components/Media';
+import MediaBody, { mdbMediaBody } from './components/MediaBody';
+import MediaImage, { mdbMediaImage } from './components/MediaImage';
+import Navbar, { mdbNavbar } from './components/Navbar';
+import mdbNavbarBrand from './components/NavbarBrand';
+import NavbarNav, { mdbNavbarNav } from './components/NavbarNav';
+import NavbarCollapse, { mdbNavbarToggler } from './components/NavbarCollapse';
+import NavbarItem, { mdbNavItem } from './components/NavbarItem';
+import PageItem, { mdbPageItem } from './components/PageItem';
+import PageNav, { mdbPageNav } from './components/PageNav';
+import Pagination, { mdbPagination } from './components/Pagination';
+import Popover, { mdbPopover } from './components/Popover';
+import ProgressBar, { mdbProgress } from './components/ProgressBar';
+import ProgressWrapper, { mdbProgressWrapper } from './components/ProgressWrapper';
+import Row, { mdbRow } from './components/Row';
+import Tbl, { mdbTable } from './components/Table';
+import TblBody, { mdbTblBody } from './components/TableBody';
+import TblHead, { mdbTblHead } from './components/TableHead';
+import Tooltip, { mdbTooltip } from './components/Tooltip';
+import ViewWrapper, { mdbView } from './components/ViewWrapper';
+import FormInline, { mdbFormInline } from './components/FormInline';
+import Modal, { mdbModal } from './components/Modal';
+import ModalHeader, { mdbModalHeader } from './components/ModalHeader';
+import ModalTitle, { mdbModalTitle } from './components/ModalTitle';
+import ModalBody, { mdbModalBody } from './components/ModalBody';
+import ModalFooter, { mdbModalFooter } from './components/ModalFooter';
+import NumericInput, { mdbNumericInput } from './components/NumericInput';
+import waves from './mixins/waves';
+
+export {
+  // FREE
+  Badge,
+  Breadcrumb,
+  BreadcrumbItem,
+  Btn,
+  BtnGroup,
+  Card,
+  CardAvatar,
+  CardUp,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  CardImg,
+  CardText,
+  CardTitle,
+  CardGroup,
+  Carousel,
+  CarouselCaption,
+  CarouselIndicator,
+  CarouselIndicators,
+  CarouselInner,
+  CarouselItem,
+  CarouselNavigation,
+  LineChart,
+  RadarChart,
+  BarChart,
+  PolarChart,
+  PieChart,
+  DoughnutChart,
+  Collapse,
+  Column,
+  Container,
+  Datatable,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  EdgeHeader,
+  Fa,
+  Footer,
+  HelloWorld,
+  Jumbotron,
+  ListGroup,
+  ListGroupItem,
+  MdInput,
+  MdMask,
+  MdTextarea,
+  Media,
+  MediaBody,
+  MediaImage,
+  Navbar,
+  NavbarNav,
+  NavbarCollapse,
+  NavbarItem,
+  PageItem,
+  PageNav,
+  Pagination,
+  Popover,
+  ProgressBar,
+  ProgressWrapper,
+  Row,
+  Tbl,
+  TblBody,
+  TblHead,
+  Tooltip,
+  ViewWrapper,
+  FormInline,
+  Modal,
+  ModalHeader,
+  ModalTitle,
+  ModalBody,
+  ModalFooter,
+  NumericInput,
+  waves,
+  // MDB FREE
+  mdbBadge,
+  mdbBreadcrumb,
+  mdbBreadcrumbItem,
+  mdbBtn,
+  mdbBtnGroup,
+  mdbCard,
+  mdbCardAvatar,
+  mdbCardUp,
+  mdbCardBody,
+  mdbCardFooter,
+  mdbCardHeader,
+  mdbCardImage,
+  mdbCardText,
+  mdbCardTitle,
+  mdbCardGroup,
+  mdbCarousel,
+  mdbCarouselCaption,
+  mdbCarouselIndicator,
+  mdbCarouselIndicators,
+  mdbCarouselInner,
+  mdbCarouselItem,
+  mdbCarouselControl,
+  mdbLineChart,
+  mdbRadarChart,
+  mdbBarChart,
+  mdbPolarChart,
+  mdbPieChart,
+  mdbDoughnutChart,
+  mdbCollapse,
+  mdbCol,
+  mdbContainer,
+  mdbDatatable,
+  mdbDropdown,
+  mdbDropdownItem,
+  mdbDropdownMenu,
+  mdbDropdownToggle,
+  mdbEdgeHeader,
+  mdbIcon,
+  mdbFooter,
+  mdbHelloWorld,
+  mdbJumbotron,
+  mdbListGroup,
+  mdbListGroupItem,
+  mdbInput,
+  mdbMask,
+  mdbTextarea,
+  mdbMedia,
+  mdbMediaBody,
+  mdbMediaImage,
+  mdbNavbar,
+  mdbNavbarBrand,
+  mdbNavbarNav,
+  mdbNavbarToggler,
+  mdbNavItem,
+  mdbPageItem,
+  mdbPageNav,
+  mdbPagination,
+  mdbPopover,
+  mdbProgress,
+  mdbProgressWrapper,
+  mdbRow,
+  mdbTable,
+  mdbTblBody,
+  mdbTblHead,
+  mdbTooltip,
+  mdbView,
+  mdbFormInline,
+  mdbModal,
+  mdbModalHeader,
+  mdbModalTitle,
+  mdbModalBody,
+  mdbModalFooter,
+  mdbNumericInput
+};
